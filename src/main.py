e0146e5063afbad58738c7595bf8fddd6502465a
sava9ecode <sava9emode@yandex.ru>
2023-12-12 17:50:30 +0300

    Add jinja2

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi_cache import FastAPICache
 # from contextlib import asynccontextmanager  # Update later
 from fastapi_cache.backends.redis import RedisBackend
@@ -9,6 +10,7 @@ from src.auth.base_config import auth_backend, fastapi_users
 from src.auth.schemas import UserRead, UserCreate
 from src.operations.router import router as operation_router
 from src.tasks.router import router as tasks_router
+from src.pages.router import router as pages_router
 from src.config import REDIS_HOST
 
 
@@ -28,6 +30,8 @@ app = FastAPI(
     # lifespan=lifespan,  # Update later
 )
 
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth",
@@ -42,6 +46,7 @@ app.include_router(
 
 app.include_router(operation_router)
 app.include_router(tasks_router)
+app.include_router(pages_router)
 
 
 origins = [
